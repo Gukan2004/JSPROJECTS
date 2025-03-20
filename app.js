444499e2ee0fd5f6be6148ad3e0691fe7357fa8b
gukan <gukanshiva39@gmail.com>
2025-03-20 13:41:50 +0530

    first commi

---
+++ app.js
@@ -0,0 +1,71 @@
+// // // let a=10;
+// // // let b='10';
+// // // console.log(a==b);
+// // // console.log(a===b);
+
+
+// // let CheckLogin =() =>{
+// //     let user=document.getElementById("u_name").value;
+    
+// //     let pass=document.getElementById("pass").value;
+// //     console.log(pass);
+// //     if (user==pass){
+// //         alert('username and password is same');
+// //     }
+
+// // }
+
+// // document.getElementById('btn').addEventListener("click",CheckLogin);
+
+// // let a=10;
+// // let b=5;
+// // console.log(a+=b);
+
+
+
+// // let age=17;
+// // let result=(age>=18)?"Eligable to vote":"Not eligable to vote";
+// // console.log(result);
+
+// // let n=10;
+// // for (let i=1; i<=10;i++){
+// //     console.log(i);
+// // }
+
+
+// // let n=10:
+// // for (let i=1;i<=n;i++){
+// //     if(i%2==0){
+        
+// //     }
+
+
+// // let n=10;
+// // let i=1;
+// // while(i<=n){
+// //     console.log(i);
+// //     i++;
+// // }
+
+
+// let n=10;
+// do{
+//     i++;
+//     console.log(i);
+
+// }while(i<=n)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
